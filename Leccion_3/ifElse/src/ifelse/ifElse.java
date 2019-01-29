cd5c0379888901ca0152f5ca5f7a80be4adaf952
vercionek <el_poca@hotmail.com>
2019-01-29 01:37:44 -0600

    RealOne

---
+++ Leccion_3/ifElse/src/ifelse/ifElse.java
@@ -0,0 +1,21 @@
+package ifelse;
+
+public class ifElse {
+
+	public static void main(String[] args) {
+		//Ejemplo If/Else If/Else
+		int x = 10;
+		if (x == 10) {
+		System.out.print("X igual a 10\n");
+		} else if (x == 20) {
+		System.out.print("X igual a 20\n");
+		} else if (x == 30) {
+		System.out.print("X igual a 30\n");
+		} else {
+		System.out.print("X no es igual ni a 10, ni 20 ni 30\n");
+		}
+
+
+	}
+
+}
